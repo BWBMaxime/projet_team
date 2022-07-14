f74b78097f05c2b37259ce33371cb24b763de679
BWBMaxime <83752129+BWBMaxime@users.noreply.github.com>
2022-07-14 12:54:57 +0200

    correction erreur last commit

--- front/src/components/table/CardVehicule.js
+++ front/src/components/table/CardVehicule.js
@@ -1,66 +1,66 @@
 const Footer = () => {
     return (
         <>
-            <div class="row pb-5 mb-4">
-                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-                    <div class="card rounded shadow-sm border-0">
-                        <div class="card-body p-4 "><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Fiat_PuntoPic.12.jpg/1200px-Fiat_PuntoPic.12.jpg" alt="" class="img-fluid d-block mx-auto mb-3"/>
-                            <h5> <a href="#" class="text-dark">Punto D'enfer</a></h5>
-                            <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                            <ul class="list-inline small">
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+            <div className="row pb-5 mb-4">
+                <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <div className="card rounded shadow-sm border-0">
+                        <div className="card-body p-4 "><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Fiat_PuntoPic.12.jpg/1200px-Fiat_PuntoPic.12.jpg" alt="" className="img-fluid d-block mx-auto mb-3"/>
+                            <h5> <a href="#" className="text-dark">Punto D'enfer</a></h5>
+                            <p className="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                            <ul className="list-inline small">
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star-o text-success"></i></li>
                             </ul>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-                    <div class="card rounded shadow-sm border-0">
-                        <div class="card-body p-4"><img src="https://i.ytimg.com/vi/lQhSmOqUW3Y/maxresdefault.jpg" alt="" class="img-fluid d-block mx-auto mb-3"/>
-                            <h5> <a href="#" class="text-dark">La Peel P50</a></h5>
-                            <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                            <ul class="list-inline small">
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <div className="card rounded shadow-sm border-0">
+                        <div className="card-body p-4"><img src="https://i.ytimg.com/vi/lQhSmOqUW3Y/maxresdefault.jpg" alt="" className="img-fluid d-block mx-auto mb-3"/>
+                            <h5> <a href="#" className="text-dark">La Peel P50</a></h5>
+                            <p className="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                            <ul className="list-inline small">
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star-o text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star-o text-success"></i></li>
                             </ul>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-                    <div class="card rounded shadow-sm border-0">
-                        <div class="card-body p-4"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToGA4VVvo75Zf51_dewnodBSH8lQweVfIilQ&usqp=CAU" alt="" class="img-fluid d-block mx-auto mb-3"/>
-                            <h5> <a href="#" class="text-dark">Epic TUK TUK</a></h5>
-                            <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <ul class="list-inline small">
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <div className="card rounded shadow-sm border-0">
+                        <div className="card-body p-4"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToGA4VVvo75Zf51_dewnodBSH8lQweVfIilQ&usqp=CAU" alt="" className="img-fluid d-block mx-auto mb-3"/>
+                            <h5> <a href="#" className="text-dark">Epic TUK TUK</a></h5>
+                            <p className="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <ul className="list-inline small">
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
                             </ul>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-                    <div class="card rounded shadow-sm border-0">
-                        <div class="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-4_vgfjy9.jpg" alt="" class="img-fluid d-block mx-auto mb-3"/>
-                            <h5> <a href="#" class="text-dark">BM Double Pied - Only for poor</a></h5>
-                            <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.orem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                            <ul class="list-inline small">
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
-                                <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <div className="card rounded shadow-sm border-0">
+                        <div className="card-body p-4"><img src="https://bootstrapious.com/i/snippets/sn-cards/shoes-4_vgfjy9.jpg" alt="" className="img-fluid d-block mx-auto mb-3"/>
+                            <h5> <a href="#" className="text-dark">BM Double Pied - Only for poor</a></h5>
+                            <p className="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.orem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                            <ul className="list-inline small">
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star text-success"></i></li>
+                                <li className="list-inline-item m-0"><i className="fa fa-star-o text-success"></i></li>
                             </ul>
                         </div>
                     </div>

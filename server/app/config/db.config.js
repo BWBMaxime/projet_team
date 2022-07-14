4403789deaab08abb23489af0220052dc4a78997
tiloic972 <tiloic972@gmail.com>
2022-07-14 18:12:30 +0200

    Delete db.config.js
    Security Reason

--- server/app/config/db.config.js
+++
@@ -1,3 +0,0 @@
-const PASSWORD = "LGxSjzJH5GUOY2pE";
-const LOGIN = "digi_team";
-export const URI = `mongodb+srv://"${LOGIN}":${PASSWORD}@cluster0.yy6y9e9.mongodb.net/?retryWrites=true&w=majority`;

c0d48587c29d236e6feb10d38b93a7c0e24766ee
geoffrey <geoffrey.petain@gmail.com>
2022-07-15 09:07:41 +0200

    gitignore config folder

--- server/app/config/db.config.js
+++
@@ -1,4 +0,0 @@
-const PASSWORD = "5EzMQolDBxdMoCYH";
-const LOGIN = "digi_team";
-const HOST = "cluster0.yy6y9e9.mongodb.net";
-export const URI = `mongodb+srv://${LOGIN}:${PASSWORD}@${HOST}/?retryWrites=true&w=majority`;

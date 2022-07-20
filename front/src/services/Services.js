80e80c0484178351eeb575120e177c51ac043e81
Célia Rojas <celia.rojas@outlook.fr>
2022-07-20 15:23:21 +0200

    fix old commit

--- front/src/services/Services.js
+++ front/src/services/Services.js
@@ -49,18 +49,93 @@ export default class Services {
       });
   };
 
-  static deleteVehicles = (idVehicle, accessToken) => {
-    const urlService = this.urlServer + "/api/vehicles/" + idVehicle;
-    return axios
-      .delete(urlService, {
-        headers: {
-          Authorization: accessToken,
-        },
-      })
-      .then((res) => {
-        return res.data;
-      });
-  };
+        static getUsers = (access_token)  => {
+                const urlService = this.urlServer+"/api/users";
+                return axios.get(urlService, {
+                        headers:{
+                                "Authorization":access_token,
+                        }
+                }).then((res)=>{
+                        // console.log(res.data)
+                        return res.data
+                })
+
+        }
+
+        static deleteUser = (id, access_token) =>{
+                const urlService = this.urlServer+"/api/users/"+id;
+                return axios.delete(urlService, {
+                        headers:{
+                                "Authorization":access_token,
+                        }
+                }).then((res)=>{
+                        console.log('supp okay')
+                })
+        }
+
+        static addUser = (login,password,lastName,firstName,profil,active,access_token)=>{
+                const urlService = this.urlServer+"/api/users";
+                const formData = {
+                        login:login,
+                        password: password,
+                        lastName : lastName,
+                        firstName : firstName,
+                        profil: profil,
+                        active: active,
+                }
+                console.log('formdatacreate',formData);
+                return axios.post(urlService, formData,{
+                        headers:{
+                                "Authorization":access_token,
+                        }
+                        
+                }).then((res)=>{
+                        console.log('Ajout effectué dans la BDD')
+                })
+        }
+
+        static updateUser = (id,login,password,lastName,firstName,profil,active,access_token)=>{
+                const urlService = this.urlServer+"/api/users/"+id;
+                const formData = {
+                        login:login,
+                        password: password,
+                        lastName : lastName,
+                        firstName : firstName,
+                        profil: profil,
+                        active: active,
+                }
+                console.log('formdata',formData);
+
+                return axios.put(urlService, formData,{
+                        headers:{
+                                "Authorization":access_token,
+                        }
+                        
+                }).then((res)=>{
+                        console.log('Modification effectué dans la BDD')
+                })
+        }
+
+        
+        static updateUserToggle = (id,login,lastName,firstName,profil,active,access_token)=>{
+                const urlService = this.urlServer+"/api/users/"+id;
+                const formData = {
+                        login:login,
+                        lastName : lastName,
+                        firstName : firstName,
+                        profil: profil,
+                        active: active,
+                }
+
+                return axios.put(urlService, formData,{
+                        headers:{
+                                "Authorization":access_token,
+                        }
+                        
+                }).then((res)=>{
+                        console.log('Modification effectué dans la BDD')
+                })
+        }
 
   static getOneVehicle = (idVehicle, accessToken) => {
     const urlService = this.urlServer + "/api/vehicles/" + idVehicle;
